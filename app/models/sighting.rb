043060786e76a75f1e4e59aaae7147d67d12ae93
Peggy Condon & Linda Gibson <student@epicodus.com>
2014-08-29 10:25:11 -0700

    validates the presence of region_id

--- app/models/sighting.rb
+++ app/models/sighting.rb
@@ -2,4 +2,5 @@ class Sighting < ActiveRecord::Base
   validates :latitude, :presence => true
   validates :longitude, :presence => true
   validates :animal_id, :presence => true
+  validates :region_id, :presence => true
 end
